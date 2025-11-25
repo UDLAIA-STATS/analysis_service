2814475198879d41b4451ace979d089fe6fb4a9d
SebastianG03 <dasedev24@gmail.com>
2025-11-24 23:33:25 -0500

    Modified requirements

--- analisis/infraestructure/services/__init__.py
+++ analisis/infraestructure/services/__init__.py
@@ -4,4 +4,3 @@ from .bbox_processor_service import (get_bbox_width, get_center_of_bbox,
                                      measure_vectorial_distance,
                                      rectangle_coords)
 from .video_processing_service import read_video, save_video
-from .utils import read_stub, save_stub
